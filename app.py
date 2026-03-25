26e9d42092010fffb08d1982e220aa19aea78d94
25aima21-Drisya <25aima21@kristujayanti.com>
2026-03-25 12:59:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for # type: ignore
+from pymongo import MongoClient # type: ignore
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Replace with your actual string or set it in Vercel Environment Variables
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://user:pass@cluster.mongodb.net/test")
+client = MongoClient(MONGO_URI)
+db = client.my_database
+
+@app.route('/')
+def index():
+    messages = db.messages.find().sort("timestamp", -1)
+    return render_template('index.html', messages=messages)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    user_text = request.form.get('content')
+    if user_text:
+        db.messages.insert_one({
+            "content": user_text,
+            "timestamp": datetime.now()
+        })
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
